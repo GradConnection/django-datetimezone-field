20aca5a793b59cb4fadb8210068ea673a187b92b
Eric Man <meric.au@gmail.com>
2013-08-29 16:45:42 +1000

    Added setup.py and __version__ to __init__.py

---
+++ setup.py
@@ -0,0 +1,47 @@
+import re
+from os import path
+from setuptools import setup
+
+
+# read() and find_version() taken from jezdez's python apps, ex:
+# https://github.com/jezdez/django_compressor/blob/develop/setup.py
+
+
+def read(*parts):
+    return open(path.join(path.dirname(__file__), *parts)).read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+setup(
+    name='django-datetimezone-field',
+    version=find_version('datetimezone_field', '__init__.py'),
+    author='Eric Man',
+    author_email='eric@gradconnection.com',
+    description=(
+        'A Django app providing database and form fields for '
+        'split datetime/time and pytz timezone objects.'
+    ),
+    long_description=read('README.md'),
+    url='https://github.com/GradConnection/django-datetimezone-field',
+    license='None',
+    packages=[
+        'datetimezone_field',
+    ],
+    install_requires=['django>=1.4.2', 'pytz'],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+        'Framework :: Django',
+    ],
+)
